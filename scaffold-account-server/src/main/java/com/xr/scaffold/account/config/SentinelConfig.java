9529270ec90c701af000de840de7a39c907a8888
杨长燕 <yang.changyan@foundbyte.com>
2020-08-11 20:10:57 +0800

    限流

--- scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelConfig.java
+++ scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelConfig.java
@@ -1,11 +1,24 @@
 package com.xr.scaffold.account.config;
 
+import com.alibaba.cloud.sentinel.SentinelProperties;
+import com.alibaba.cloud.sentinel.datasource.config.DataSourcePropertiesConfiguration;
+import com.alibaba.cloud.sentinel.datasource.config.NacosDataSourceProperties;
 import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
-import com.alibaba.csp.sentinel.adapter.servlet.config.WebServletConfig;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * <b>@author</b>: yang.changyan@foundbyte.com
@@ -14,6 +27,9 @@ import javax.annotation.PostConstruct;
  */
 @Configuration
 public class SentinelConfig {
+
+  @Autowired
+  private SentinelProperties sentinelProperties;
 
   @Bean
   public UrlBlockHandler urlBlockHandler(){
@@ -25,7 +41,20 @@ public class SentinelConfig {
    */
   @PostConstruct
   public void init(){
-    WebServletConfig.setBlockPage("http://www.baidu.com");
+//    WebServletConfig.setBlockPage("http://www.baidu.com");
+
+    Properties properties = null;
+    NacosDataSourceProperties ndsp = null;
+    Map<String, DataSourcePropertiesConfiguration> dataSource = sentinelProperties.getDatasource();
+    for(Map.Entry<String, DataSourcePropertiesConfiguration> ds : dataSource.entrySet()){
+      ndsp = ds.getValue().getNacos();
+      properties = new Properties();
+      properties.setProperty("namespace", ndsp.getNamespace());
+      properties.setProperty("serverAddr", ndsp.getServerAddr());
+      Converter<String, List<FlowRule>> parser = source -> JSON.parseObject(source,new TypeReference<List<FlowRule>>() {});
+      ReadableDataSource<String, List<FlowRule>> nacosDataSource = new NacosDataSource<>(properties, ndsp.getGroupId(), ndsp.getDataId(), parser);
+      FlowRuleManager.register2Property(nacosDataSource.getProperty());
+    }
   }
 
 }
