4b141750afd62d4aaa2f42ba0dab23d9d9f13e2c
杨长燕 <yang.changyan@foundbyte.com>
2020-09-02 15:55:18 +0800

    spring cloud gateway动态路由

--- scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelConfig.java
+++ scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelConfig.java
@@ -1,60 +1,60 @@
-package com.xr.scaffold.account.config;
-
-import com.alibaba.cloud.sentinel.SentinelProperties;
-import com.alibaba.cloud.sentinel.datasource.config.DataSourcePropertiesConfiguration;
-import com.alibaba.cloud.sentinel.datasource.config.NacosDataSourceProperties;
-import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
-import com.alibaba.csp.sentinel.datasource.Converter;
-import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
-import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-/**
- * <b>@author</b>: yang.changyan@foundbyte.com
- * <b>@time</b>: 2020-08-11 14:11:00
- * <b>@description</b>:
- */
-@Configuration
-public class SentinelConfig {
-
-  @Autowired
-  private SentinelProperties sentinelProperties;
-
-  @Bean
-  public UrlBlockHandler urlBlockHandler(){
-    return new SentinelUrlBlockHandler();
-  }
-
-  /**
-   *
-   */
-  @PostConstruct
-  public void init(){
-//    WebServletConfig.setBlockPage("http://www.baidu.com");
-
-    Properties properties = null;
-    NacosDataSourceProperties ndsp = null;
-    Map<String, DataSourcePropertiesConfiguration> dataSource = sentinelProperties.getDatasource();
-    for(Map.Entry<String, DataSourcePropertiesConfiguration> ds : dataSource.entrySet()){
-      ndsp = ds.getValue().getNacos();
-      properties = new Properties();
-      properties.setProperty("namespace", ndsp.getNamespace());
-      properties.setProperty("serverAddr", ndsp.getServerAddr());
-      Converter<String, List<FlowRule>> parser = source -> JSON.parseObject(source,new TypeReference<List<FlowRule>>() {});
-      ReadableDataSource<String, List<FlowRule>> nacosDataSource = new NacosDataSource<>(properties, ndsp.getGroupId(), ndsp.getDataId(), parser);
-      FlowRuleManager.register2Property(nacosDataSource.getProperty());
-    }
-  }
-
-}
+//package com.xr.scaffold.account.config;
+//
+//import com.alibaba.cloud.sentinel.SentinelProperties;
+//import com.alibaba.cloud.sentinel.datasource.config.DataSourcePropertiesConfiguration;
+//import com.alibaba.cloud.sentinel.datasource.config.NacosDataSourceProperties;
+//import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+//import com.alibaba.csp.sentinel.datasource.Converter;
+//import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+//import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+//import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+//import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.TypeReference;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.annotation.PostConstruct;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Properties;
+//
+///**
+// * <b>@author</b>: yang.changyan@foundbyte.com
+// * <b>@time</b>: 2020-08-11 14:11:00
+// * <b>@description</b>:
+// */
+//@Configuration
+//public class SentinelConfig {
+//
+//  @Autowired
+//  private SentinelProperties sentinelProperties;
+//
+//  @Bean
+//  public UrlBlockHandler urlBlockHandler(){
+//    return new SentinelUrlBlockHandler();
+//  }
+//
+//  /**
+//   *
+//   */
+//  @PostConstruct
+//  public void init(){
+////    WebServletConfig.setBlockPage("http://www.baidu.com");
+//
+//    Properties properties = null;
+//    NacosDataSourceProperties ndsp = null;
+//    Map<String, DataSourcePropertiesConfiguration> dataSource = sentinelProperties.getDatasource();
+//    for(Map.Entry<String, DataSourcePropertiesConfiguration> ds : dataSource.entrySet()){
+//      ndsp = ds.getValue().getNacos();
+//      properties = new Properties();
+//      properties.setProperty("namespace", ndsp.getNamespace());
+//      properties.setProperty("serverAddr", ndsp.getServerAddr());
+//      Converter<String, List<FlowRule>> parser = source -> JSON.parseObject(source,new TypeReference<List<FlowRule>>() {});
+//      ReadableDataSource<String, List<FlowRule>> nacosDataSource = new NacosDataSource<>(properties, ndsp.getGroupId(), ndsp.getDataId(), parser);
+//      FlowRuleManager.register2Property(nacosDataSource.getProperty());
+//    }
+//  }
+//
+//}
--- scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelUrlBlockHandler.java
+++ scaffold-account-server/src/main/java/com/xr/scaffold/account/config/SentinelUrlBlockHandler.java
@@ -1,30 +1,30 @@
-package com.xr.scaffold.account.config;
-
-import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
-import com.xr.base.core.dto.ResultDto;
-import com.xr.base.core.util.JsonUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-/**
- * <b>@author</b>: yang.changyan@foundbyte.com
- * <b>@time</b>: 2020-08-11 14:14:00
- * <b>@description</b>:
- */
-public class SentinelUrlBlockHandler implements UrlBlockHandler {
-
-  private final String defaultBlockResult = JsonUtils.toJson(ResultDto.failure("系统繁忙，请稍后重试。"));
-
-  @Override
-  public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
-    if(e instanceof FlowException){
-      httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      httpServletResponse.setContentType("application/json;charset=utf-8");
-      httpServletResponse.getWriter().write(defaultBlockResult);
-    }
-  }
-}
+//package com.xr.scaffold.account.config;
+//
+//import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+//import com.alibaba.csp.sentinel.slots.block.BlockException;
+//import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+//import com.xr.base.core.dto.ResultDto;
+//import com.xr.base.core.util.JsonUtils;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * <b>@author</b>: yang.changyan@foundbyte.com
+// * <b>@time</b>: 2020-08-11 14:14:00
+// * <b>@description</b>:
+// */
+//public class SentinelUrlBlockHandler implements UrlBlockHandler {
+//
+//  private final String defaultBlockResult = JsonUtils.toJson(ResultDto.failure("系统繁忙，请稍后重试。"));
+//
+//  @Override
+//  public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
+//    if(e instanceof FlowException){
+//      httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//      httpServletResponse.setContentType("application/json;charset=utf-8");
+//      httpServletResponse.getWriter().write(defaultBlockResult);
+//    }
+//  }
+//}
