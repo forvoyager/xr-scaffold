b0026c74e482550e700950daa5596e8649026bcb
杨长燕 <yang.changyan@foundbyte.com>
2020-09-27 19:45:05 +0800

    配置调整

--- scaffold-gateway-server/src/main/java/com/xr/scaffold/config/SwaggerDocumentProvider.java
+++ scaffold-gateway-server/src/main/java/com/xr/scaffold/config/SwaggerDocumentProvider.java
@@ -1,13 +1,12 @@
 package com.xr.scaffold.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.config.GatewayProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,32 +18,18 @@ import java.util.List;
 @Component
 public class SwaggerDocumentProvider implements SwaggerResourcesProvider {
 
-  @Autowired
-  private RouteLocator routeLocator;
-
-  @Autowired
-  private GatewayProperties gatewayProperties;
+  @Resource
+  private RouteDefinitionLocator routeDefinitionLocator;
 
   @Override
   public List<SwaggerResource> get() {
     List<SwaggerResource> resources = new ArrayList<>();
 
-    List<String> routes = new ArrayList<>();
-    routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
-
-    gatewayProperties.getRoutes().stream()
-            .filter(routeDefinition -> routes.contains(routeDefinition.getId()))
-            .forEach(routeDefinition -> {
-              routeDefinition.getPredicates().stream()
-                      .filter(predicateDefinition -> "Path".equalsIgnoreCase(predicateDefinition.getName()))
-                      .forEach(predicateDefinition -> {
-                        resources.add(swaggerResource(
-                                routeDefinition.getId(),
-                                predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("/**", "/v2/api-docs"),
-                                "2.0"
-                        ));
-                      });
-            });
+    routeDefinitionLocator.getRouteDefinitions().subscribe(
+            routeDefinition -> routeDefinition.getPredicates().forEach(
+                    predicateDefinition -> resources.add(
+                            this.swaggerResource(routeDefinition.getId(), predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("/**", "/v2/api-docs"), "2.0")
+                    )));
 
     return resources;
   }
